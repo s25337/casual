00d9ebf1759010830a013f45a5aecf3a48301ebc
s25337 <92116309+s25337@users.noreply.github.com>
2021-12-07 00:25:32 +0100

    pointers and arrays

--- 3.3.0.4.cpp
+++ 3.3.0.4.cpp
@@ -1,29 +1,33 @@
 #include <cstdio>
 #include <iostream>
-using namespace std;
 
 void fpprint(void* a[], size_t n, void (*fp)(void*)) {
 
-for(int i=0; i<n; i++) (*fp)(a[i]);
+for(int i=0; i<n; i++){
+	std::cout<<i<<" = ";
+	(*fp)(a[i]);
+}
 
 }
 
 void printint(void* elem) {
-	auto m=elem;
-	
-std::cout<<m<<"\n";
+	int* ptr = (int*)elem;
+	std::cout<<*ptr<<"\n";
 }
-//void printstring(void* elem) {
-//std::cout<<elem<<"\n";
-//}
+void printstring(void* elem) {
+	std::string* ptr = (std::string*)elem;
+	std::cout<<*ptr<<"\n";
+}
 
 int main(int argc, char const* argv[])
 {
 	//char a[]="lol";
-	int a=5,b=3,c=6;
-	
-	void* t[]={&a, &b, &c};
+	int a[]= {5, 3, 6};
+	std::string b[]={"hello","world",":("};
+	void* t[]={&a[0], &a[1], &a[2]};
+	void* s[]={&b[0], &b[1], &b[2]};
 	size_t size=3;
 	fpprint(t,size,printint);
+	fpprint(s,size,printstring);
     return 0;
 }
